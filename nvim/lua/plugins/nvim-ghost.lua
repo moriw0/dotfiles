1794a73869afc4cbeb5c75f16e25ab0555f36c1f
Shota Morinaga <shota.morinaga@as-child.com>
2025-06-04 22:09:20 +0900

    esa もマークダウン形式で GhostText する

--- nvim/lua/plugins/nvim-ghost.lua
+++ nvim/lua/plugins/nvim-ghost.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     vim.api.nvim_create_augroup("nvim_ghost_user_autocommands", { clear = true })
     vim.api.nvim_create_autocmd("User", {
-      pattern = { "*github.com" },
+      pattern = { "*github.com", "*esa.io" },
       group = "nvim_ghost_user_autocommands",
       callback = function()
         vim.opt.filetype = "markdown"
