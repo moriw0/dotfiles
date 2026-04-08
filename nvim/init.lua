9d112ec89c490da52a81e7518449d716841b9cf9
Shota Morinaga <shota.morinaga@as-child.com>
2026-04-08 13:41:59 +0900

    neovimの設定を変更して、行の折り返しを有効にしました。

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ vim.cmd("source " .. vim.fn.expand("~/dotfiles/vim/common.vim"))
 
 -- nvim 固有設定
 vim.opt.relativenumber = true
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 8
